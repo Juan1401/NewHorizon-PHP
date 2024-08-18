263e871803a44cfe655c7cc834a30155eef80f7d
Juan1401 <jjquitiaquez@gmail.com>
2024-08-18 10:08:48 -0500

    Nuevo Curso - Arreglos

---
+++ Curso_Arreglos_Funciones_Estructuras_de_Control/1-arreglos.php
@@ -0,0 +1,8 @@
+<?php
+
+// $edades = [20, 18, 40];   // 1ra manera de escribir un arreglo.
+$edades = array(20, 18, 40); // 2da manera de escribir un arreglo.
+
+echo "Una de las edades que esta dentro es " . $edades[2];
+
+echo "\n";
