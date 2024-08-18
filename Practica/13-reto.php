a86e7a688dd343c09655244327461be501dae339
Juan1401 <jjquitiaquez@gmail.com>
2024-08-17 23:28:38 -0500

    se arregla versionamiento del punto 13

--- Practica/13-reto.php
+++ Practica/13-reto.php
@@ -1,22 +1,22 @@
 <?php
-// /* RETO 2
-// Hacer un programa que convierta,horas,minutos y segundos unicamente a Segundos [hh:mm:ss] */
+/* RETO 2
+Hacer un programa que convierta,horas,minutos y segundos unicamente a Segundos [hh:mm:ss] */
 
-// $reto2 = readline("Ingresa la hora con el siguiente formato: 'hh:mm:ss' ");
-// // echo($reto2);
+$reto2 = readline("Ingresa la hora con el siguiente formato: 'hh:mm:ss' ");
+// echo($reto2);
 
-// //que se genere un posición en un objeto cada vez que encuentre un ':'
-// $horariodivido = explode(":",$reto2 );
-// // var_dump($horariodivido);
+//que se genere un posición en un objeto cada vez que encuentre un ':'
+$horariodivido = explode(":",$reto2 );
+// var_dump($horariodivido);
 
-// $horas = $horariodivido[0];
-// $minutos = $horariodivido[1];
-// $segundos = $horariodivido[2];
+$horas = $horariodivido[0];
+$minutos = $horariodivido[1];
+$segundos = $horariodivido[2];
 
-// $horas = (int)$horas*3600;
-// $minutos = (int)$minutos*60;
-// $sumatoria = (int)$horas+$minutos+$segundos;
+$horas = (int)$horas*3600;
+$minutos = (int)$minutos*60;
+$sumatoria = (int)$horas+$minutos+$segundos;
 
-// echo ("La hora ingresada: " . $reto2 . " hace referencia a: " . $sumatoria . " segundos");
+echo ("La hora ingresada: " . $reto2 . " hace referencia a: " . $sumatoria . " segundos");
 
 ?>
