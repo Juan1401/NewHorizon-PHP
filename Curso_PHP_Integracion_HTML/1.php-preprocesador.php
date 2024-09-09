11154d6b377430df5e2c1f81187cb17cf658deb9
Juan1401 <jjquitiaquez@gmail.com>
2024-09-08 22:00:10 -0500

    Preprocesador de HTML

---
+++ Curso_PHP_Integracion_HTML/1.php-preprocesador.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HTML desde PHP!</title>
+</head>
+<body>  
+    <h1>PHP como preprocesador de HTML 😈</h1>
+    <?php echo "<p>Ahora esto se esta impriendo con PHP 💕</p>"; ?>
+
+
+</body>
+</html>
