26da1475c097d5acf3cdca7c176b36447a91f08f
Juan1401 <jjquitiaquez@gmail.com>
2024-09-29 15:17:38 -0500

    Proyecto de composer con phpunit

---
+++ 6.Curso_Manejo_de_Datos/5.Proyecto/tests/ValidateTest.php
@@ -0,0 +1,17 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use App\Validate;
+
+
+class ValidateTest extends TestCase
+{
+    public function test_email()
+    {
+        $email = Validate::email('jjquitiaquez@gmail.com');
+        $this->assertTrue($email);
+
+        $email = Validate::email('jjquitiaquez@@gmail.com');
+        $this->assertFalse($email);
+    }
+}
