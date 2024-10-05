f68d77ee5ae45e04bb32d8c6fefa91b32400dc2a
Juan1401 <jjquitiaquez@gmail.com>
2024-10-04 23:09:53 -0500

    chequeo adicional de indices.

--- 6.Curso_Manejo_de_Datos/12.comparación.php
+++ 6.Curso_Manejo_de_Datos/12.comparación.php
@@ -2,7 +2,7 @@
 
 $courses = ['javascript','php'];
 $wishes = ['php','laravel','javascript','vuejs'];
-
+echo '<pre>';
 
 //MIRAR LAS DIFERENCIAS
 //Muestra que datos son diferentes entre los 2 arreglos.
@@ -14,9 +14,18 @@ echo "<br>";
 $arrayA = [1, 2, 3, 4, 5];
 $arrayB = [3, 4, 5, 6, 7];
 
+
 var_dump(array_diff($arrayA, $arrayB));
+echo "<br>";
+
 //Evalua depende al orden que coloquemos
 var_dump(array_diff($arrayB, $arrayA));
+echo "<br>";
+
 
 #array_diff_assoc()
 //Calcula la diferencia entre arrays con un chequeo adicional de índices
+$arrayA1 = array("a" => "JavaScript", "b" => "PHP", "c" => "Golang", "d" => "Ruby");
+$arrayB1 = array("a" => "JavaScript", "d" => "Haskell", "Assembler");
+var_dump(array_diff_assoc($arrayA1, $arrayB1));
+//output: ['PHP', 'Golang', 'Ruby']
