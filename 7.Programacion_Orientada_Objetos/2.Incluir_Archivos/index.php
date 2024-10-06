b6affc40b21afa99715a62e95610cf4b0e6f6021
Juan1401 <jjquitiaquez@gmail.com>
2024-10-05 20:11:04 -0500

    comment

--- 7.Programacion_Orientada_Objetos/2.Incluir_Archivos/index.php
+++ 7.Programacion_Orientada_Objetos/2.Incluir_Archivos/index.php
@@ -6,7 +6,7 @@
 //** Da un error FATAL si no encuentra el archivo (o no existe) y detiene el sistema.
 // require 'greet.php';
 
-//** */ Evita que tengamos un error si llamamos varias veces al archivo con una "REQUIRE" */
+//? Evita que tengamos un error si llamamos varias veces al archivo con una "REQUIRE" */
 require_once 'greets.php';
 
 echo greets('Juan Jose','Trabaja duro y seras lo que te propones...');
