64812e4b66645abe0114796ccb06f0612828070f
Juan1401 <jjquitiaquez@gmail.com>
2024-10-09 20:42:18 -0500

    Abstracción por medio un clase

---
+++ 7.Programacion_Orientada_Objetos/4.Abstraccion.php/class/Auth.php
@@ -0,0 +1,23 @@
+<?php
+
+
+class Auth
+{
+
+    // Es describir en esencia lo que yo requiero para mi clase.
+    protected $email;
+    protected $password;
+
+    public function login()
+    {
+
+    }
+
+    public function validate()
+    {
+        
+    }
+
+    //Failed, reponse
+
+}
