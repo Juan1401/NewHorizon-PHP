a570a78e27090a623516f8250f0b25da9cd2945d
Juan1401 <jjquitiaquez@gmail.com>
2024-10-26 17:13:58 -0500

    Ejemplo de PHP DOC

---
+++ 8.Entornos_Virtuales_Funciones_Avanzadas/1.php_doc.php
@@ -0,0 +1,18 @@
+<?php 
+
+/**
+ * Función de suma
+ *
+ * @param int $n1
+ * @param int $n2
+ * @return int
+ */
+function sumar($n1, $n2){
+    return $n1 + $n2;
+}
+
+$resultado = sumar(10,2);
+echo $resultado;
+
+
+?>
