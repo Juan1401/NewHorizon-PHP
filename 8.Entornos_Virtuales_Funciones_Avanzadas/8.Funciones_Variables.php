af935fdc99e4ce6e7517cf76dfd3e0403798ee19
Juan1401 <jjquitiaquez@gmail.com>
2024-10-26 20:52:12 -0500

    Funciones Variables y reto

---
+++ 8.Entornos_Virtuales_Funciones_Avanzadas/8.Funciones_Variables.php
@@ -0,0 +1,17 @@
+<?php
+
+function michi(){
+    echo "Meow";
+}
+
+function dogo(){
+    echo "Woof!";
+}
+
+function zorro(){
+    echo "Zorro no te lo lleves!";
+}
+
+$function = "zorro";
+
+$function();
