332470081be292efc2e004e911ae1dd857762834
Juan1401 <jjquitiaquez@gmail.com>
2024-10-31 23:10:14 -0500

    Funciones para manipulación de fechas y manejo de Objetos DateTime

--- 9.Cookies_Sesiones_Modularizacion/5.date/Funciones-fechas.php
+++ 9.Cookies_Sesiones_Modularizacion/5.date/Funciones-fechas.php
@@ -27,6 +27,7 @@ echo "\n";
 $unix_time_last_wednesday = strtotime("last Wednesday");
 echo "\n";
 echo date("Y-m-d H:i:s", $unix_time_last_wednesday); 
+echo "\n\n";
 
 /*Fechas inmutables */
 //$date_inmutable = new DateTimeImmutable();
@@ -34,8 +35,29 @@ echo date("Y-m-d H:i:s", $unix_time_last_wednesday);
 
 
 //Calcular diferencias de tiempo.
+//Saber el intervalo de tiempo de nacmiento de MrMichi.
 
+echo "Saber los años de mr Michi: \n";
+$today = new Datetime();
+$mrmichi_birth = new DateTime("2020-10-30");
+$interval = $mrmichi_birth->diff($today);
+
+echo $interval->format("%y años con %d dias");
+echo "\n\n";
+
+// Crear desde un formato dado
+// Pasar una fecha en formato de texto a un formato de fecha dado.
+echo "Creando una fecha con base a un formato dado:\n";
+$date = DateTime::createFromFormat("l j F Y","Sunday 17 April 2022");
+
+//Formateo a mi formato de preferencia.
+echo $date->format("Y-m-d H:i:s");
+echo "\n\n";
   
+//Modificar una fecha 
+echo "Modificando una fecha con la función modify: \n";
+$date = new DateTime(); //Fecha de Hoy
+$date->modify("+1day + 2months"); // Se le añadieron 1 dia mas 2 meses a la fecha actual.
+echo $date->format("Y-m-d"); //Resultado.
 
-
-?>
+?>    
