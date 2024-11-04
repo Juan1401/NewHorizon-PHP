6738a0e1a3fbda763000448c9b581810a41530f3
Juan1401 <jjquitiaquez@gmail.com>
2024-11-04 16:01:24 -0500

    Refactor realiazdo por mi, para convertir la conexión PDO en un clase con patron de diseño Singleton

--- 10.Curso_PHP_Bases_de_Datos/Conexion_MySQLi/Database/PDO/Connection.php
+++ 10.Curso_PHP_Bases_de_Datos/Conexion_MySQLi/Database/PDO/Connection.php
@@ -1,13 +1,38 @@
 <?php
 
-$server = "phpmyadmin.test"; //Virtual Host asigando para php my admin.
-$database = "finanzas_personales";
-$username = "juaquihu";
-$password = "Naranja-1401";
+namespace Database\PDO;
 
-$connection = new PDO("mysql:dbname=$database;host=$server", $username, $password);
+class Connection {
 
-$setnames = $connection->prepare("SET NAMES 'utf8'");
-$setnames->execute();
+    private static $instance;
+    private $connection;
 
-var_dump($setnames);
+    private function __construct() {
+        $this->make_connection();
+    }
+
+    private static function getInsances(){
+        if(!self::$instance instanceof self)
+        {
+            self::$instance = new self();
+        }
+    }
+    
+    public function get_database_instance(){
+        return $this->connection;
+    }
+
+    function make_connection(){
+        $server = "phpmyadmin.test"; //Virtual Host asigando para php my admin.
+        $database = "finanzas_personales";
+        $username = "juaquihu";
+        $password = "Naranja-1401";
+
+        $connection = new \PDO("mysql:dbname=$database;host=$server", $username, $password);
+
+        $setnames = $connection->prepare("SET NAMES 'utf8'");
+        $setnames->execute();
+
+       $this->connection = $connection;
+    }
+}
