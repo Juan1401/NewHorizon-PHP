26e984eff38d5c01df19e53c1c5a17c2a1a550c9
Juan1401 <jjquitiaquez@gmail.com>
2024-11-04 18:13:19 -0500

    Inserción de datos a BD mediante controlador incomes y mandando información en un arreglo

---
+++ 10.Curso_PHP_Bases_de_Datos/PHP-DATABASE/index.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Controllers\IncomesController;
+use App\Enums\IncomeTypeEnum;
+use App\Enums\PaymentMethodEnum;
+
+require "vendor/autoload.php";
+
+
+$incomes_controller = new IncomesController();
+$incomes_controller->store([
+    "payment_method" => PaymentMethodEnum::BankAccount->value,
+    "type" => IncomeTypeEnum::Salary->value,
+    "date" => date("Y-m-d H:i:s"), //2022-06-14 20:18:18;
+    "amount" => 10000000,
+    "description" => "Pago de mi salario por mi arduo y muy buen trabajo :D"
+]);
+
+
+
+
+
